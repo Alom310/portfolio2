04c72bbe666e0069deb2fb6716d5b20409b5e87a
alom <alom310@gmail.com>
2020-02-10 16:23:29 -0800

    edit php file

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,26 +2,29 @@
     // require 'phpmailer/Exception.php'
     // require 'phpmailer/PHPmailer.php'
     // require 'phpmailer/SMTP.php'
-
-    require ('vendor/autoload.php');
-
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception; 
 
+    require 'vendor/autoload.php';
+
+try {
+    //Server settings
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     $mail->SMTPAuth   = true;
     $mail->Host = 'smtp.gmail.com';
-    $mail-> SMTPSecure = 'tls';
-    $mail-> Post = 587;
-    $mail-> usename = 'alomrelay@gmail.com';
-    $mail-> password = 'Relay1234$';
-    $mail-> setForm('alomrelay@gmail.com');
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->Username = 'alomrelay@gmail.com';
+    $mail->Password = 'Relay1234$';
 
+    //Recipients
     $mail->setFrom($_POST['email'],$_POST['name']);
     $mail->addAddress('alomrelay@gmail.com');
     $mail->addReplyTo($_POST['email'],$_POST['name']);
+
+    // Content
     $mail->isHTML(true);
     $mail->Subject='Form Submission:' .$_POST['subject'];
     $mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h3>';
@@ -33,7 +36,5 @@
     {
         $result="Thanks\t" .$_POST['name']. " for contacting us.";
     }
-
-    $mail->smtpClose();
-
+}
 ?>
