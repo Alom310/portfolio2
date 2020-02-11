5c760d3cabcccfc93d9cc214685358f485d72d63
alom <alom310@gmail.com>
2020-02-10 16:30:48 -0800

    edit php file

--- contact.php
+++ contact.php
@@ -1,3 +1,44 @@
+<?php
+    // require 'phpmailer/Exception.php'
+    // require 'phpmailer/PHPmailer.php'
+    // require 'phpmailer/SMTP.php'
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception; 
+
+    require 'vendor/autoload.php';
+
+try {
+    //Server settings
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->SMTPAuth   = true;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->Username = 'alomrelay@gmail.com';
+    $mail->Password = 'Relay1234$';
+
+    //Recipients
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress('alomrelay@gmail.com');
+    $mail->addReplyTo($_POST['email'],$_POST['name']);
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject='Form Submission:' .$_POST['subject'];
+    $mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h3>';
+    if(!$mail->send())
+    {
+        $error = "Something went worng. Please try again.";
+    }
+    else 
+    {
+        $result="Thanks\t" .$_POST['name']. " for contacting us.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -90,7 +131,7 @@
           </div>
           <div class="row block-9">
             <div class="col-md-6 pr-md-5">
-              <form action="contact-form-handler.php" method="post" id="contact-form">
+              <form action="" method="post" id="contact-form">
                 <div class="form-group">
                   <input type="text" name="name" class="form-control" placeholder="Your Name">
                 </div>
